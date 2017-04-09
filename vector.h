a2ffd768d34a93db1a67bc9d590812c449256bcf
Evelek <majkel196@onet.pl>
2017-04-09 22:34:16 +0200

    Update vector.h
    Poprawa algorytmu dodania oraz usunięciu wartości vectora.
    Dodanie capacity oraz konstruktora kopiującego i operatora przypisania.

--- vector.h
+++ vector.h
@@ -8,76 +8,203 @@
 
 template <class Type>
 class Vector {
-	Type *tab;
-	size_t rozmiar;
+private:
+	Type *main_vector;
+	size_t vector_size;
+	size_t capacity;
 public:
-	Vector();
-	void dodaj_element(const Type &wartosc);
-	void usun_element();
-	void wyswietl_vector() const;
-	size_t zwroc_rozmiar() const;
-	void wyczysc_vector();
-	void sortowanie_rosnaco();
-	void sortowanie_malejaco();
+	Vector();															// Vector <Type> object;
+	Vector(const size_t vector_size_) : vector_size(vector_size_) {}	// Vector <Type> object(size_t);
+	Vector(const Vector &);												// Vector <Type> new_object(current_object);
+	Vector const operator=(const Vector &);								// Vector <Type> new_object = current_object;
+	~Vector();
+
+	void add_new_element(const Type &value);
+	void remove_element();
+	void show_vector() const;
+	size_t size() const;
+	void reset_vector();
+	void sort_ascending();
+	void sort_descending();
 };
 
 template<class Type>
 Vector<Type>::Vector() {
-	tab = new Type[1];
-	rozmiar = 0;
+	capacity = 10;
+	main_vector = new Type[capacity];
+	vector_size = 0;
+	for (size_t i = 0; i < capacity; ++i)
+		main_vector[i] = 0;
 }
 
 template<class Type>
-void Vector<Type>::dodaj_element(const Type &wartosc) {
-	Type *tablica_tymczasowa = new Type[rozmiar]; //utworzenie tymczasowej tablicy dynamicznej
-	for (size_t i = 0; i < rozmiar; i++)
-		tablica_tymczasowa[i] = tab[i];  //skopiowanie wartosci z glownej tablicy do tymczasowej
-	delete[] tab; //dealokacja pamieci tablicy glownej
-	tab = new Type[rozmiar + 1]; //alokacja pamieci nowej powiekszonej glownej tablicy
-	for (size_t i = 0; i < rozmiar; i++)
-		tab[i] = tablica_tymczasowa[i];  //skopiowanie wartosci z tablicy tymczasowej do glownej
-	tab[rozmiar] = wartosc; //dodanie elementu do glownej tablicy
-	rozmiar++;
-	delete[] tablica_tymczasowa; //dealokacja pamieci tablicy tymczasowej
+Vector<Type>::Vector(const Vector &copy) {
+	capacity = copy.capacity;
+	vector_size = copy.vector_size;
+	main_vector = new Type[capacity];
+	for (size_t i = 0; i < vector_size; ++i)
+		main_vector[i] = std::copy(copy.main_vector[i]);
 }
 
 template<class Type>
-void Vector<Type>::usun_element() {
-	rozmiar--;
-	Type *tablica_tymczasowa = new Type[rozmiar]; //utworzenie tymczasowej tablicy dynamicznej
-	for (size_t i = 0; i < rozmiar; i++)
-		tablica_tymczasowa[i] = tab[i];  //skopiowanie wartosci z glownej tablicy do tymczasowej bez ostatniego elementu
-	tab = new (tab) Type[rozmiar]; //miejscowa wersja new
-	for (size_t i = 0; i < rozmiar; i++)
-		tab[i] = tablica_tymczasowa[i];  //skopiowanie wartosci z tablicy tymczasowej do glownej
-	delete[] tablica_tymczasowa; //dealokacja pamieci tablicy tymczasowej
+Vector const Vector<Type>::operator=(const Vector &copy)
+{
+	if (this == &copy)
+		return *this;
+
+	delete[] main_vector;
+	capacity = copy.capacity;
+	vector_size = copy.vector_size;
+	main_vector = new Type[capacity];
+	for (size_t i = 0; i < vector_size; ++i)
+		main_vector[i] = std::copy(copy.main_vector[i]);
+	return *this;
 }
 
 template<class Type>
-void Vector<Type>::wyswietl_vector() const {
-	for (size_t i = 0; i < rozmiar; ++i)
-		std::cout << tab[i] << " ";
+Vector<Type>::~Vector() {
+	delete[] main_vector;
 }
 
 template<class Type>
-size_t Vector<Type>::zwroc_rozmiar() const {
-	return rozmiar;
+void Vector<Type>::add_new_element(const Type &value) {
+	if (vector_size < capacity) {
+		main_vector[vector_size] = value;
+		vector_size++;
+	}
+	else {
+	    /* Description:
+		   1) Alocate memory for temp_copy_vector
+		   2) Copy elements from main_vector to temp_copy_vector
+		   3) Dealocate main_vector's memory
+		   4) Alocate new memory for main_vector
+		   5) Increase capacity by 10
+		   6) Copy elements from temp_copy_vector to main_vector
+		   7) Dealocate temp_copy_vector's memory
+		*/
+
+		capacity += 10; //increase capacity by 10
+
+		Type *temp_copy_vector = new Type[vector_size]; //alocate memory for temp_copy_vector which is destination for copying elements from main_vector
+		Type *pointer = &main_vector[vector_size]; //point one-past-the-end element of main_vector
+		size_t pointer_moves = 0; //variable counts temp_copy_vector's and main_vector's moves during copying elements
+
+		//copy elements from main_vector to temp_copy_vector
+		while (main_vector != pointer) {
+			*temp_copy_vector++ = std::move(*main_vector++);
+			pointer_moves++;
+		};
+		pointer = &temp_copy_vector[0]; //point the last element of temp_copy_vector
+
+		//move back main_vector and temp_copy_vector on initial position
+		for (size_t i = 0; i < pointer_moves; ++i) {
+			main_vector--;
+			temp_copy_vector--;
+		}
+
+		delete[] main_vector; //dealocate main_vector's memory
+
+		vector_size++;
+		main_vector = new Type[capacity]; //alocate memory for main_vector
+
+		//copy elements from temp_copy_vector to main_vector
+		while (temp_copy_vector != pointer) {
+			*main_vector++ = std::move(*temp_copy_vector++);
+		}
+
+		main_vector[0] = value; //add element to the main_vector
+		
+		//move back main_vector and temp_copy_vector on initial position
+		for (size_t i = 0; i < pointer_moves; ++i) {
+			main_vector--;
+			temp_copy_vector--;
+		}
+
+		delete[] temp_copy_vector; //dealocate temp_copy_vector's memory
+	}
 }
 
 template<class Type>
-void Vector<Type>::wyczysc_vector() {
-	delete[] tab;
-	rozmiar = 0;
+void Vector<Type>::remove_element() {
+	if (vector_size < capacity - 10) { //if vector_size is smaller than capacity - 10
+		vector_size--;
+		main_vector[vector_size] = 0;
+	}
+	else{
+		/* Description:
+		1) Alocate memory for temp_copy_vector
+		2) Copy elements from main_vector to temp_copy_vector
+		3) Dealocate main_vector's memory
+		4) Alocate new memory for main_vector
+		5) Increase capacity by 10
+		6) Copy elements from temp_copy_vector to main_vector
+		7) Dealocate temp_copy_vector's memory
+		*/
+		capacity -= 10; //increase capacity by 10
+		vector_size--;  //reduction vector_size by one
+
+		Type *temp_copy_vector = new Type[vector_size]; //alocate memory for temp_copy_vector which is destination for copying elements from main_vector
+		Type *pointer = &main_vector[vector_size]; //point one-past-the-end element of main_vector
+		size_t pointer_moves = 0; //variable counts temp_copy_vector's and main_vector's moves during copying elements
+
+		//copy elements from main_vector to temp_copy_vector
+		while (main_vector != pointer) {
+			*temp_copy_vector++ = std::move(*main_vector++);
+			pointer_moves++;
+		}
+		pointer = &temp_copy_vector[0]; //point the last element of temp_copy_vector
+
+		//move back main_vector and temp_copy_vector on initial position
+		for (size_t i = 0; i < pointer_moves; ++i) {
+			main_vector--;
+			temp_copy_vector--;
+		}
+
+		main_vector = new (main_vector)Type[vector_size]; //placement new
+
+		//copy elements from temp_copy_vector to main_vector
+		while (temp_copy_vector != pointer) {
+			*main_vector++ = std::move(*temp_copy_vector++);
+		}
+
+		//move back main_vector and temp_copy_vector on initial position
+		for (size_t i = 0; i < pointer_moves; ++i) {
+			main_vector--;
+			temp_copy_vector--;
+		}
+
+		delete[] temp_copy_vector; //dealocate temp_copy_vector's memory
+	}
 }
 
 template<class Type>
-void Vector<Type>::sortowanie_rosnaco() {
-	std::sort(tab, tab + rozmiar, std::less<Type>());
+void Vector<Type>::show_vector() const {
+	for (size_t i = 0; i < vector_size; ++i)
+		std::cout << main_vector[i] << " ";
 }
 
 template<class Type>
-void Vector<Type>::sortowanie_malejaco() {
-	std::sort(tab, tab + rozmiar, std::greater<Type>());
+size_t Vector<Type>::size() const {
+	return vector_size;
+}
+
+template<class Type>
+void Vector<Type>::reset_vector() {
+	delete[] main_vector;
+	vector_size = 0;
+	capacity = 10;
+}
+
+template<class Type>
+void Vector<Type>::sort_ascending()
+{
+	std::sort(main_vector, main_vector + vector_size, std::less<Type>());
+}
+
+template<class Type>
+void Vector<Type>::sort_descending()
+{
+	std::sort(main_vector, main_vector + vector_size, std::greater<Type>());
 }
 
 #endif // !VECTOR_H_
